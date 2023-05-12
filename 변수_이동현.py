acb2eb01c4285e472f31960617a132cb2ea29f13
dongggggggggggggg <131439316+dongggggggggggggg@users.noreply.github.com>
2023-05-12 16:59:52 +0900

    Add files via upload

---
+++ 변수_이동현.py
@@ -0,0 +1,28 @@
+# 2023.5.12
+'''
+a=float(input("a를 입력하세요:"))
+b=int(input("b를 입력하세요:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a/b)
+'''
+'''
+a=int(input("a를 입력하세요:"))
+b=int(input("b를 입력하세요:"))
+
+print("a=%d 입니다,b=%d입니다"%(a,b))
+'''
+name="이동현"
+age=14
+height=167
+print("제이름은 이동현입니다")
+
+hope=int(input("희망하는 키를 작성 해봐:"))
+print("제이름은 %s입니다.\n 나이는 %d, 희망키는 %d."%(name,age,height))
+print("제이름은 {}입니다.\n나이는 {}, 희망키는 {}.".format(name,age,height))
+
+tmp=hope-height
+
+print("그럼 앞으로 %d 만큼 커야겠네요 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ"%(hope-height))
