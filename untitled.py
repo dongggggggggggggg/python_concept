34ac848d5a248415e740eb401a13ec18ac099ea4
dongggggggggggggg <131439316+dongggggggggggggg@users.noreply.github.com>
2023-06-30 16:42:41 +0900

    반복문(for)

---
+++ untitled.py
@@ -0,0 +1,24 @@
+'''
+a="Life is too short, you need pyhon"
+if "wife" in a: print(wife)
+elif "python" in a and  "you" not in a: print("python")
+elif "shirt" not in a: print("shrit")
+elif "need" in a: print("need")
+else: print("none")
+'''
+'''
+result = 0
+i=1
+while i<=1000:
+    if i%3==0:
+        result=result+i
+    i=i+1
+print(result)
+'''
+'''
+result=0
+for i in range(1,1001,1):
+     if i%3==0:
+         result=result+i
+print(result)
+'''
